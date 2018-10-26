699d4d9cf8b3243a17ffdc361a1fc3ca52e1fcba
admin <shibaoping2017@163.com>
2018-10-26 10:53:46 +0800

    sbp:The relational data source module verifies whether the database can be connected

--- trunk/drsr/src/main/java/cn/csdb/drsr/controller/RelationSourceController.java
+++ trunk/drsr/src/main/java/cn/csdb/drsr/controller/RelationSourceController.java
@@ -99,7 +99,7 @@ public class RelationSourceController {
         datasrc.setDataSourceId(dataSourceId1);
         logger.info("测试新增或编辑的数据能否连通数据库");
 
-        String flag = relationShipService.testCon(host,port,userName,"",dataBaseName);
+        String flag = relationShipService.testCon(host,port,userName,password,dataBaseName);
 
         if(flag=="success"){
             return relationShipService.editData(datasrc);
