86078cd743ddac545033276d97283eb300162b1c
zhangzl419 <zhangzl419@163.com>
2018-10-26 14:26:26 +0800

    reset trunk/drsr/target

---
+++ trunk/drsr/src/main/java/cn/csdb/drsr/service/SubjectAdminLoginService.java
@@ -0,0 +1,27 @@
+package cn.csdb.drsr.service;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SubjectAdminLoginService
+{
+    public int validateLogin(String userName, String password)
+    {
+        //1、访问中心端验证登录是否成功
+
+        //2、如果登录成功，验证专题库信息是否同步，如果没有同步下来，则同步一次
+        //localhost:8080/api/getSubject/{subjectCode}
+        //   /api/getSubject/{subjectCode}
+        getSubjectConfig();
+
+
+        return  0;
+    }
+
+    private boolean getSubjectConfig()
+    {
+        //JSONObject subjectObject = RestTemplate.
+        return true;
+    }
+}
