346e5dfd24eb06f0e22290ace28750a779c406fa
username <shibaoping2017@163.com>
2018-11-22 18:38:25 +0800

    shibaoping:Increase log function

--- trunk/drsr/src/main/java/cn/csdb/drsr/controller/DataSyncController.java
+++ trunk/drsr/src/main/java/cn/csdb/drsr/controller/DataSyncController.java
@@ -65,6 +65,7 @@ public class DataSyncController {
     @ResponseBody
     @RequestMapping("/ftpUpload")
     public int ftpUpload(int dataTaskId,String processId){
+        logger.info("=========================上传流程开始========================" + "\n");
         String host = configPropertyService.getProperty("FtpHost");
         String userName = configPropertyService.getProperty("FtpUser");
         String password = configPropertyService.getProperty("FtpPassword");
