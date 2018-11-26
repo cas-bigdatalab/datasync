dd7507b877be200405d92a43d8d41433de182dfc
hwalfa <346308740@qq.com>
2018-11-26 15:47:02 +0800

    add "未完成" status

--- trunk/portal/src/main/java/cn/csdb/portal/controller/ResourceController.java
+++ trunk/portal/src/main/java/cn/csdb/portal/controller/ResourceController.java
@@ -289,7 +289,7 @@ public class ResourceController {
         resource.setPublishOrgnization(publishOrganization);
         resource.setCreateOrgnization(createOrganization);
         resource.setCreatePerson(createPerson);
-        resource.setStatus("1");
+        resource.setStatus("-1");
         String resourceId = resourceService.save(resource);
         jsonObject.put("resourceId", resourceId);
         return jsonObject;
@@ -346,7 +346,7 @@ public class ResourceController {
             resource.setFilePath(sb.toString().replace("/", "%_%"));
             resource.setToMemorySize(String.valueOf(size));
         }
-//        resource.setResState("未完成");
+        resource.setStatus("-1");
         String resId = resourceService.save(resource);
         jsonObject.put("resourceId", resId);
         return jsonObject;
@@ -371,7 +371,7 @@ public class ResourceController {
         JSONObject jsonObject = new JSONObject();
         cn.csdb.portal.model.Resource resource = resourceService.getById(resourceId);
         resource.setUserGroupId(userGroupIdList);
-//        resource.setResState("待审核");
+        resource.setStatus("1");
         String resId = resourceService.save(resource);
         jsonObject.put("resourceId", resId);
         return jsonObject;
@@ -449,7 +449,12 @@ public class ResourceController {
         resource.setPublishOrgnization(publishOrganization);
         resource.setCreateOrgnization(createOrganization);
         resource.setCreatePerson(createPerson);
-        resource.setStatus("1");
+        if(StringUtils.isNotBlank(resource.getUserGroupId())){
+            resource.setStatus("1");
+        }else{
+            resource.setStatus("-1");
+        }
+
         String resId = resourceService.save(resource);
         jsonObject.put("resourceId", resId);
         return jsonObject;
@@ -503,7 +508,11 @@ public class ResourceController {
             resource.setFilePath(sb.toString().replace("/", "%_%"));
             resource.setToMemorySize(String.valueOf(size));
         }
-//        resource.setResState("未完成");
+        if(StringUtils.isNotBlank(resource.getUserGroupId())){
+            resource.setStatus("1");
+        }else{
+            resource.setStatus("-1");
+        }
         String resId = resourceService.save(resource);
         jsonObject.put("resourceId", resId);
         return jsonObject;
@@ -528,7 +537,7 @@ public class ResourceController {
         JSONObject jsonObject = new JSONObject();
         cn.csdb.portal.model.Resource resource = resourceService.getById(resourceId);
         resource.setUserGroupId(userGroupIdList);
-//        resource.setResState("待审核");
+        resource.setStatus("1");
         String resId = resourceService.save(resource);
         jsonObject.put("resourceId", resId);
         return jsonObject;
