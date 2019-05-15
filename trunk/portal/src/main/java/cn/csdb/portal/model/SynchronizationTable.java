b7c71bbc55832fdb648860ffffc783128414398f
mengjinbao <780410997@qq.com>
2019-05-15 15:58:39 +0800

    增加保存 查询全部 方法。

--- trunk/portal/src/main/java/cn/csdb/portal/model/SynchronizationTable.java
+++ trunk/portal/src/main/java/cn/csdb/portal/model/SynchronizationTable.java
@@ -5,13 +5,19 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 @Document(collection = "t_synchronization_tables")
-public class SynchronizationTables {
+public class SynchronizationTable {
     @Id
     private String id;
     @Field("tableName")
     private String tableName; //同步表名
     @Field("subjectCode")
     private String subjectCode;
+    @Field("frequency")
+    private Long frequency; // 同步频次 毫秒值
+    @Field("sqlString")
+    private String sqlString; // 关联创建时的 DDL
+    @Field("lastModify")
+    private Long lastModify; // 最后一次修改的时间 毫秒值
 
     public String getId() {
         return id;
@@ -35,5 +41,29 @@ public class SynchronizationTables {
 
     public void setSubjectCode(String subjectCode) {
         this.subjectCode = subjectCode;
+    }
+
+    public Long getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(Long frequency) {
+        this.frequency = frequency;
+    }
+
+    public String getSqlString() {
+        return sqlString;
+    }
+
+    public void setSqlString(String sqlString) {
+        this.sqlString = sqlString;
+    }
+
+    public Long getLastModify() {
+        return lastModify;
+    }
+
+    public void setLastModify(Long lastModify) {
+        this.lastModify = lastModify;
     }
 }
--- trunk/portal/src/main/java/cn/csdb/portal/repository/SynchronizationTablesDao.java
+++ trunk/portal/src/main/java/cn/csdb/portal/repository/SynchronizationTablesDao.java
@@ -1,12 +1,14 @@
 package cn.csdb.portal.repository;
 
-import cn.csdb.portal.model.SynchronizationTables;
+import cn.csdb.portal.common.SynchronizeTable;
+import cn.csdb.portal.model.SynchronizationTable;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 
 @Repository
@@ -15,6 +17,15 @@ public class SynchronizationTablesDao {
     @Resource
     private MongoTemplate mongoTemplate;
 
+    public void save(SynchronizeTable synchronizeTable) {
+        mongoTemplate.save(synchronizeTable);
+    }
+
+    public List<SynchronizationTable> selectAll() {
+        List<SynchronizationTable> all = mongoTemplate.findAll(SynchronizationTable.class);
+        return all;
+    }
+
     /**
      * @Description: 根据表名和subjectCode查询该表是否同步
      * @Param: [subjectCode, tableName]
@@ -22,8 +33,8 @@ public class SynchronizationTablesDao {
      * @Author: zcy
      * @Date: 2019/5/15
      */
-    public SynchronizationTables selectOneBySubjectCodeAndTableName(String subjectCode, String tableName) {
-        SynchronizationTables synchronizationTables = mongoTemplate.findOne(new Query(Criteria.where("subjectCode").is(subjectCode).and("tableName").is(tableName)), SynchronizationTables.class);
+    public SynchronizationTable selectOneBySubjectCodeAndTableName(String subjectCode, String tableName) {
+        SynchronizationTable synchronizationTables = mongoTemplate.findOne(new Query(Criteria.where("subjectCode").is(subjectCode).and("tableName").is(tableName)), SynchronizationTable.class);
         return synchronizationTables;
     }
 }
