a0417b4aad9bec1d1784a35bcc99bbc5ca02d8d0
shibaoping <shibaoping2017@163.com>
2019-06-13 15:32:41 +0800

    shibaoping: Bug fix.

--- trunk/portal/src/main/java/cn/csdb/portal/utils/dataSrc/DruidUtil.java
+++ trunk/portal/src/main/java/cn/csdb/portal/utils/dataSrc/DruidUtil.java
@@ -50,7 +50,7 @@ public class DruidUtil {
         prop.setProperty("driverClassName","com.mysql.jdbc.Driver");
         prop.setProperty("validationQuery","SELECT 1 FROM DUAL");
         prop.setProperty("url","jdbc:mysql://"+host+":"+port+"/"+databaseName);
-        prop.setProperty("connectionProperties","Unicode=true;characterEncoding=UTF-8;UseSSL=false");
+        prop.setProperty("connectionProperties","zeroDateTimeBehavior=convertToNull;Unicode=true;characterEncoding=UTF-8;UseSSL=false");
         prop.setProperty("username",username);
         prop.setProperty("password",password);
         prop.setProperty("initialSize","3");
